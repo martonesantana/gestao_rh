1a2a10bc019172ff64534161f7de85c59b0f502f
martonesantana <martonesantos@hotmail.com>
2022-10-21 10:44:28 -0300

    Relacionando as tabelas

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,10 +1,13 @@
 from tabnanny import verbose
 from django.db import models
 
+from apps.funcionarios.models import Funcionario
+
 # Create your models here.
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=200, help_text='Descrição do documento')
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     
     class Meta:
         verbose_name = 'Documento'
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,17 @@
 from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
+
+
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 # Create your models here.
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do funcionário')
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
     
     class Meta:
         verbose_name = 'Funcionário'
